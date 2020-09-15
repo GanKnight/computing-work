b353abb0aff383a9ca1e7f7cb3e4fc0ece62fa78
GanKnight <69836902+GanKnight@users.noreply.github.com>
2020-09-15 22:03:46 +0800

    Create spot the error 4.py

---
+++ micropython/spot the error 4.py
@@ -0,0 +1,9 @@
+from Microbit Import**
+print("Roll the 12 sided die")
+while True:
+  if button_b.was_pressed or button_b.was_pressed:
+    display.scroll(random.randint(1,12)
+    
+#There are multiple errors
+#You can choose to fix one question max.
+  
