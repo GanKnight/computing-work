15ea34956bbe59dbbf16d3bbd9593a1bc8c6eb86
GanKnight <69836902+GanKnight@users.noreply.github.com>
2020-09-17 20:07:20 +0800

    Update spot the error 4.py

--- micropython/spot the error 4.py
+++ micropython/spot the error 4.py
@@ -3,7 +3,24 @@ print("Roll the 12 sided die")
 while True:
   if button_b.was_pressed or button_b.was_pressed:
     display.scroll(random.randint(1,12)
+                   
+# qn 2
+from microbit* import
+
+while count< 3:
+  display.show(Image.RABBIT)
+  displa.show(Image.Sword)
+  if button_b was pressed():
+    display.show(Image.BROKEN_HEART)
+    count-=1
+    elif button_b.was pressed():
+      display.show(Image.HAPPY)
+    count-=1
+        
     
+    if count =0:
+      displau.croll("You died")
+      display.show(Image.SKULL                   
 #There are multiple errors
 #You can choose to fix one question max.
   
