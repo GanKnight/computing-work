79437ee26c7f32bb6ccccc0644795a9659995600
GanKnight <69836902+GanKnight@users.noreply.github.com>
2020-09-17 19:57:56 +0800

    Update spot the error 2.py

--- micropython/spot the error 2.py
+++ micropython/spot the error 2.py
@@ -7,7 +7,29 @@ while True:
     display.scroll(random.randint(1,6)
    time=time-1
    elif time=0:
-    display.clear
+ display.clear
+                   
+ # part 2               
+
+from microbit import*
+count=1
+while true:
+    WHile count=0:
+    
+    for button.a_is_pressed():
+      start = running_time()
+    elif accelerometer.was_gesture() ==False:
+      display.scroll(IMAGE.skull)
+      end=running_time()
+      count=count+1
+      time=start*end
+      display.scroll(time
+  
+                   
+                   
+              
+                   
+                   
     
  #There are multiple errors
 #You can choose to fix one question max. 
